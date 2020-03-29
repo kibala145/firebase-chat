00c15b1e1afb9dbf4f001c47caedbb14ae563c1d
ikibala <kibalaprogramming@gmail.com>
2020-03-29 18:51:05 +0200

    build

--- src/firebaseConfig.js
+++
@@ -1,7 +0,0 @@
-// Get a RTDB instance
-import firebase from 'firebase/app'
-import 'firebase/database'
-
-export const firebaseData = firebase
-  .initializeApp({ databaseURL: 'https://chat-room-eed70.firebaseio.com' })
-  .database()
